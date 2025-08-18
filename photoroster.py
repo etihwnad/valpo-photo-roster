9c076f72b60c7c1ad361c0105d001b3f1c69f11b
Dan White <dan@whiteaudio.com>
2025-08-18 16:57:25 -0500

    Use proxy

--- photoroster.py
+++ photoroster.py
@@ -19,8 +19,14 @@ from wtforms.validators import DataRequired, NumberRange, Optional
 
 from werkzeug.utils import secure_filename
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 app.secret_key = 'foo'
 csrf.CSRFProtect(app)
